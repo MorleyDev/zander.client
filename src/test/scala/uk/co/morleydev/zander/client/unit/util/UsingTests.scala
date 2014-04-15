f7f26294295760be8599f5cbc27a302d3bb1bf26
MorleyDev <jason@morleydev.co.uk>
2014-04-15 03:03:20 +0100

    Added throw test to Using

--- src/test/scala/uk/co/morleydev/zander/client/unit/util/UsingTests.scala
+++ src/test/scala/uk/co/morleydev/zander/client/unit/util/UsingTests.scala
@@ -6,15 +6,16 @@ import uk.co.morleydev.zander.client.util.Using
 class UsingTests extends FunSpec {
 
   describe("Given an autoclosable") {
-    val closable = new AutoCloseable {
+    class MockAutoCloseable {
       var wasClosed = false
       def close() : Unit = {
         wasClosed = true
       }
     }
-
     describe("When using a using block") {
-      var actualClosable : AutoCloseable = null
+      val closable = new MockAutoCloseable()
+
+      var actualClosable : MockAutoCloseable = null
       Using.using(closable) { c =>
         actualClosable = c
       }
@@ -23,7 +24,27 @@ class UsingTests extends FunSpec {
         assert(actualClosable == closable)
       }
       it("Then the autoclosable was closed") {
-        assert(closable.wasClosed == true)
+        assert(closable.wasClosed)
+      }
+    }
+    describe("When using a using block that throws") {
+
+      val expectedException = new Exception()
+      var actualException : Exception = null
+      val closable = new MockAutoCloseable()
+      try {
+        Using.using(closable) { c =>
+          throw expectedException
+        }
+      } catch {
+        case e : Exception => actualException = e
+      }
+
+      it("Then the expected exception was thrown") {
+        assert(actualException == expectedException)
+      }
+      it("Then the autoclosable was closed") {
+        assert(closable.wasClosed)
       }
     }
   }
