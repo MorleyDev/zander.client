1263a0acb52fb7bfd642d01c882bb5cae49faf4e
MorleyDev <jason@morleydev.co.uk>
2014-04-22 18:58:51 +0100

    Added improved logging

--- src/main/scala/uk/co/morleydev/zander/client/service/impl/LocalArtefactPurge.scala
+++ src/main/scala/uk/co/morleydev/zander/client/service/impl/LocalArtefactPurge.scala
@@ -7,15 +7,20 @@ import uk.co.morleydev.zander.client.model.arg.BuildCompiler.BuildCompiler
 import uk.co.morleydev.zander.client.data.{DeleteProjectArtefacts, DeleteProjectArtefactDetails, ReadProjectArtefactDetails}
 import java.io.FileNotFoundException
 import uk.co.morleydev.zander.client.service.exception.NoLocalArtefactsExistException
+import uk.co.morleydev.zander.client.util.Log
 
 class LocalArtefactPurge(getDetails : ReadProjectArtefactDetails,
                          deleteDetails : DeleteProjectArtefactDetails,
                          deleteArtefacts : DeleteProjectArtefacts) extends PurgeProjectArtefacts {
   override def apply(p: Project, c: BuildCompiler, m: BuildMode): Unit = {
+
+    Log.message("Purging artefacts for %s %s %s".format(p,c,m))
+
     val details = try {
       getDetails(p, c, m)
     } catch {
       case e: FileNotFoundException =>
+        Log.error("Artefacts not installed")
         throw new NoLocalArtefactsExistException()
     }
     deleteDetails(p, c, m)
--- src/main/scala/uk/co/morleydev/zander/client/util/Log.scala
+++ src/main/scala/uk/co/morleydev/zander/client/util/Log.scala
@@ -2,6 +2,6 @@ package uk.co.morleydev.zander.client.util
 
 object Log {
   def message(v1 : Any) = println(v1)
-  def warning(v1 : Any) = println(Console.YELLOW + "[WARN] " + v1 + Console.RESET)
-  def error(v1 : Any) = println(Console.RED + "[ERR] " + Console.RESET)
+  def warning(v1 : Any) = println("%s[WARN] %s%s".format(Console.YELLOW, v1, Console.RESET))
+  def error(v1 : Any) = println("%s[ERR] %s%s".format(Console.RED, v1, Console.RESET))
 }
