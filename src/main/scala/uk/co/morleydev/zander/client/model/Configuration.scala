93554d343d6a539ce8f18b561c1f82522d7f99cd
Jason Morley <morleydev.co.uk@gmail.com>
2014-07-21 02:30:01 +0100

    Changed default server to http://zander.morleydev.co.uk/api

--- src/main/scala/uk/co/morleydev/zander/client/model/Configuration.scala
+++ src/main/scala/uk/co/morleydev/zander/client/model/Configuration.scala
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import java.io.File
 import uk.co.morleydev.zander.client.util.GetZanderHomeDirectory
 
-case class Configuration(@JsonProperty("server") server : String = "http://zander.morleydev.co.uk",
+case class Configuration(@JsonProperty("server") server : String = "http://zander.morleydev.co.uk/api",
                          @JsonProperty("programs") programs : ProgramConfiguration = new ProgramConfiguration(),
                          @JsonProperty("cache") cache : String = new File(GetZanderHomeDirectory(), "cache").getAbsolutePath)
